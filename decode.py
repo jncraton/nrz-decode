4d7b182abb9f90a10a7ee963232f732fa861b814
Jon Craton <jncraton@gmail.com>
2021-09-16 12:16:38 -0400

    Update structure

--- decode.py
+++ decode.py
@@ -1,68 +1,108 @@
 import wave
 import struct
-import bitarray
 import sys
 
-def wav_values(wavfile):
-    """ Implements a generator to return raw waveform values from a wav file
 
-    Raw values will be yielded as a floating-point value from -1 to 1
+def get_samples(wavfile):
+    """Returns a list of raw waveform values from a wav file
 
-    >>> f"{next(wav_values('message.wav')):0.2f}"
+    Raw values will be returned as floating-point values from -1 to 1
+
+    For simplicity, this function need only support wav files up to 1 MiB
+
+    >>> f"{get_samples('message.wav')[0]:0.2f}"
     '-0.09'
 
-    >>> f"{list(wav_values('message.wav'))[320]:0.2f}"
+    >>> f"{get_samples('message.wav')[320]:0.2f}"
     '0.83'
     """
-    
-    with wave.open(wavfile, 'rb') as w:
-        for pos in range(0, sys.maxsize):
-            frame = w.readframes(1)
-            if not frame:
-                break
-            yield struct.unpack('<h', frame)[0] / 2 ** 15
 
-def decode_bits(wavfile):
-    """ Implements a generator to return a decoded bitstream from wavfile 
 
-    >>> next(decode_bits('message.wav'))
+def decode_bits(samples):
+    """Returns a list of bits decoded from wave file samples
+
+    >>> decode_bits(get_samples('message.wav'))[0]
     0
-    
-    >>> list(decode_bits('message.wav'))[0:10]
-    [0, 0, 1, 1, 1, 0, 0, 1, 0, 1]
 
-    Hint: enumerate can be used to iterate over the values in the file including a numeric index
+    >>> decode_bits(get_samples('message.wav'))[2]
+    1
+
+    >>> decode_bits(get_samples('message.wav'))[0:10]
+    [0, 0, 1, 1, 1, 0, 0, 1, 0, 1]
     """
 
-def bits_to_string(bits):
-    """ Generator to map an iterator of bits to an iterator of characters
 
-    >>> next(bits_to_string(iter([0, 0, 1, 1, 1, 0, 0, 1, 0, 1])))
+def get_char(bits):
+    """Converts a list of 8 bits to a Python character
+
+    >>> get_char([0, 1, 1, 1, 0, 0, 1, 0])
     'N'
 
-    >>> next(bits_to_string(iter([1, 0, 1, 1, 1, 0, 0, 1, 0, 1])))
+    >>> get_char([0, 0, 0, 0, 1, 0, 1, 0])
+    'P'
+
+    >>> get_char([0, 1, 0, 0, 1, 1, 1, 0])
+    'r'
+    """
+
+
+def parse_byte(byte):
+    """Processes 1 byte of data (10 total bits) off the bitstream
+
+    Note that byte here does not refer to the Python byte data type. A byte
+    here refers to a 10 bit sequence including the stop and start bit on
+    the wire.
+
+    The stop and start bits should be verified before the string is returned.
+
+    >>> parse_byte([0, 0, 1, 1, 1, 0, 0, 1, 0, 1])
+    'N'
+
+    >>> parse_byte([1, 0, 1, 1, 1, 0, 0, 1, 0, 1])
     Traceback (most recent call last):
     ...
     ValueError: Invalid start bit
 
-    >>> next(bits_to_string(iter([0, 0, 1, 1, 1, 0, 0, 1, 0, 0])))
+    >>> parse_byte([0, 0, 1, 1, 1, 0, 0, 1, 0, 0])
     Traceback (most recent call last):
     ...
     ValueError: Invalid stop bit
-
-    Hint: bitarray can be used to convert bits to bytes
-        https://pypi.org/project/bitarray/
-    Hint2: Raw byte values can be converted to strings using `bytes.decode`:
-        https://docs.python.org/3/library/stdtypes.html#bytes.decode
     """
 
+
+def decode_string(bits):
+    """Creates a string from a list of raw bits
+
+    >>> decode_string([0, 0, 1, 1, 1, 0, 0, 1, 0, 1])
+    'N'
+
+    >>> decode_string([0, 0, 1, 1, 1, 0, 0, 1, 0, 1]*3)
+    'NNN'
+
+    >>> next(decode_string([1, 0, 1, 1, 1, 0, 0, 1, 0, 1]))
+    Traceback (most recent call last):
+    ...
+    ValueError: Invalid start bit
+
+    >>> next(decode_string([0, 0, 1, 1, 1, 0, 0, 1, 0, 0]))
+    Traceback (most recent call last):
+    ...
+    ValueError: Invalid stop bit
+    """
+
+
 def decode(wavfile):
-    """ Decode a wav file containing an NRZ encoded message
+    """Decode a wav file containing an NRZ encoded message
 
     >>> ''.join(decode('message.wav'))
     'Now you know how to decode messages transmitted using non-return-to-zero encoding!'
     """
 
-if __name__ == '__main__':
-    print(''.join(decode(sys.argv[1])))
-        
+    samples = get_samples(wavfile)
+    bits = decode_bits(samples)
+
+    return decode_string(bits)
+
+
+if __name__ == "__main__":
+    print("".join(decode(sys.argv[1])))
